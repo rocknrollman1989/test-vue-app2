fa9c6bc8280f73e4ca4fb9bce04bfed26711aa09
Ilia Volkov <ilia.volkov@sagaris-soft.com>
2019-02-12 10:36:57 +0200

    init Vue app, add: axios, ANT, tests-Unit, ets

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue';
+import Button from 'ant-design-vue/lib/button';
+import 'ant-design-vue/dist/antd.css';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
 
+Vue.component(Button.name, Button);
 Vue.config.productionTip = false;
 
 new Vue({
